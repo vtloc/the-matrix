47f3cd0ce7418d35dd834a10a7def0874b773169
vtloc <vtloc1989@gmail.com>
2018-04-06 17:07:37 +0800

    fix code to pass the tests

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(flash());
+// app.use(flash());
 
 
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
